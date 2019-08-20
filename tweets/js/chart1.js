960bb31f950c067391849593c00c69c1035b49e3
gsvevo <g.svevo@gmail.com>
2019-08-19 22:42:58 -0700

    tweets

--- tweets/js/chart1.js
+++ tweets/js/chart1.js
@@ -1,33 +1,42 @@
-var count = [
-    22,
-    21, 
-    17,
-    16,    
-    10,   
-    ]
+var items = [];
+var labels = [];
+$.getJSON("assets/list.json", function() {
+    
+  }).then(function(data) {
 
+    $.each( data, function( key, val ) {
+        labels.push(key);
+        items.push(val );
+      });
 
+Chart.defaults.global.defaultFontFamily = "Lustria";
+//var count = [22, 21, 17, 16, 10,]
+var ctx = document.getElementById('canvas').getContext('2d');
+var purple_orange_gradient = ctx.createLinearGradient(0, 0, 0, 600);
+purple_orange_gradient.addColorStop(0, 'rgba(22, 160, 133, 0.7)');
+purple_orange_gradient.addColorStop(1, 'rgba(245, 229, 27, 0.7)');
 var barChartData = {
-labels: ['migrant','people', 'dead', 'help', 'world'],
+//labels: ['migrant','people', 'dead', 'help', 'world'],
+labels: labels,
 datasets: [{
     label: 'Frequency',
-    backgroundColor: window.chartColors.green,
+    fontFamily: 'Lustria',
+    backgroundColor: purple_orange_gradient,
+	hoverBackgroundColor: purple_orange_gradient,
+	hoverBorderWidth: 2,
+	hoverBorderColor: 'rgba(22, 160, 133, 0.3)',
     stack: 'Stack 0',
-    data: count,
-    fontFamily: 'Lustria'
+    data: items,
+    
 }]
-
 };
-
-var ctx = document.getElementById('canvas').getContext('2d');
 window.myBar = new Chart(ctx, {
     type: 'horizontalBar',
     data: barChartData,
     options: {
         title: {
             display: true,
-            text: 'Most used words in today\'s tweets',
-            fontFamily: 'Lustria'
+            text: 'Most frequent words'
         },
         layout: {
             padding: {
@@ -39,8 +48,7 @@ window.myBar = new Chart(ctx, {
         },
         tooltips: {
             mode: 'index',
-            intersect: true,
-            fontFamily: 'Lustria'
+            intersect: true
         },
         responsive: true,
         scales: {
@@ -54,4 +62,4 @@ window.myBar = new Chart(ctx, {
         }
     }
 });
-
+});
--- tweets/js/chart2.js
+++ tweets/js/chart2.js
@@ -1,62 +1,79 @@
-var count = [
-    1,
-    0,
-    2,
-    18,
-    7,
-    45,
-    21,
-    18,
-    11,
-    3,
-    1
-    ]
+var count =[]
 
+$.getJSON("assets/sentiment.json", function() {
+  
+}).then(function(data) {
 
-var sentimentChartData = {
-labels: ['-1', '-0.8', '-0.6', '-0.4', '-0.2', '0', '+0.2', '+0.4', '+0.6', '+0.8', '+1'],
-datasets: [{
-    label: 'Tweets',
-    fontFamily: 'Lustria',
-    backgroundColor: Samples.utils.transparentize(window.chartColors.blue),
-    stack: 'Stack 0',
-    data: count
-}]
+  $.each( data, function(i) {
+      count.push(data[i]);
+    });
+Chart.defaults.global.defaultFontFamily = "Lustria";
+var ctx = document.getElementById('canvas2').getContext("2d");
 
-};
-
-var ctx2 = document.getElementById('canvas2').getContext('2d');
-new Chart(ctx2, {
+var myChart = new Chart(ctx, {
+    plugins: [
+    {
+      id: "responsiveGradient",
+      afterLayout: function(chart, options) {
+        var scales = chart.scales;
+        // create a linear gradient with the dimentions of the scale
+        var color = chart.ctx.createLinearGradient(
+          scales["x-axis-0"].left,
+          scales["y-axis-0"].bottom,
+          scales["x-axis-0"].right,
+          scales["y-axis-0"].top
+        );
+        // add gradients stops
+        color.addColorStop(0, "#f49080");
+        color.addColorStop(0.49, "#ffffff");
+        color.addColorStop(1, "#80b6f4");
+        // changes the background color option
+        chart.data.datasets[0].backgroundColor = color;
+      }
+    }
+  ],
     type: 'line',
-    data: sentimentChartData,
+    data: {
+        labels: ['-1', '-0.8', '-0.6', '-0.4', '-0.2', '0', '+0.2', '+0.4', '+0.6', '+0.8', '+1'],
+        datasets: [{
+            label: "Tweets",
+            data: count
+        }]
+    },
     options: {
         title: {
             display: true,
-            text: 'Distribution of Tweets based on Sentiment Polarity (-1 = negative +1 = positive)',
-            fontFamily: 'Lustria'
+            text: 'Distribution of Tweets based on Sentiment Polarity (-1 = negative +1 = positive)'
+            
         },
-        layout: {
-            padding: {
-                left: 20,
-                right: 50,
-                top: 10,
-                bottom: 40
-            }
+        legend: {
+            position: "bottom"
         },
-        tooltips: {
-            mode: 'index',
-            intersect: true
-        },
-        responsive: true,
         scales: {
-            xAxes: [{
-                stacked: true
-            }],
             yAxes: [{
-                stacked: true,
-                //barThickness: 20,
+                ticks: {
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold",
+                    beginAtZero: true,
+                    maxTicksLimit: 5,
+                    padding: 20
+                },
+                gridLines: {
+                    drawTicks: false,
+                    display: false
+                }
+            }],
+            xAxes: [{
+                gridLines: {
+                    zeroLineColor: "transparent"
+                },
+                ticks: {
+                    padding: 20,
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold"
+                }
             }]
         }
     }
+})
 });
-
